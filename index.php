61fde612b454dedbf5d5c11254b7ea86bbb5aa5a
Malathrom <adrian.genin@live.fr>
2017-05-20 17:24:35 +0200

    ajout de boites colorées

--- index.php
+++ index.php
@@ -4,13 +4,65 @@
 
     <div class="mainText">
         <h1>Bienvenue sur UTT parcours.</h1>
-        <p class="lead">Entrez votre parcours.<br>Ajouter un étudiant à la base de données :
-            <a href="addStudent.php" class="btn btn-lg btn-danger">Ajouter étudiant</a><br>
-
-        </p>
     </div>
 
-</div>
+    <div class="forceCenter">
+        <div class="row">
+            <div class="formTaille">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Ajouter un étudiant à la base de données</h3>
+                    </div>
+                    <div class="panel-body">
+                        <a href="addStudent.php" class="btn btn-lg btn-danger">Ajouter étudiant</a><br>
+                    </div>
+                </div>
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Panel title</h3>
+                    </div>
+                    <div class="panel-body">
+                        Panel content
+                    </div>
+                </div>
 
+
+                <div class="panel panel-success">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Panel title</h3>
+                    </div>
+                    <div class="panel-body">
+                        Panel content
+                    </div>
+                </div>
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Panel title</h3>
+                    </div>
+                    <div class="panel-body">
+                        Panel content
+                    </div>
+                </div>
+
+
+                <div class="panel panel-warning">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Panel title</h3>
+                    </div>
+                    <div class="panel-body">
+                        Panel content
+                    </div>
+                </div>
+                <div class="panel panel-danger">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Panel title</h3>
+                    </div>
+                    <div class="panel-body">
+                        Panel content
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
