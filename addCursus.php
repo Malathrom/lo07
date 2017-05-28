7921b896a872c4a311666e4f4e4f380578d0fd23
Malathrom <adrian.genin@live.fr>
2017-05-28 18:55:17 +0200

    dd

--- addCursus.php
+++ addCursus.php
@@ -2,91 +2,100 @@
 
 <?php include 'header.php'; ?>
 
+
+
+<script type='text/javascript' src="cursus.js"></script>
+
+
+
+
 <div class="container">
 
-  <div class="mainText">
-      <h1>Votre parcours</h1>
-      <div class="well" style="text-align: left">
-          <p>
-Numéro Semestre: numéro de votre semestre à l'UTT</br>
-Label Semestre: sigle d'une branche(TC,ISI,SRT,...) et numéro du semestre dans cette branche
-Sigle: sigle d’une UE, label d’un stage, ...</br>
-Catégorie:CS, TM, EC,HT, ME, ST, SE, HP, NPML,...</br>
-Affectation: à quelle partie du cursus doivent être affectés les crédits associés à l’élément de formation</br>
-Utt?: fait à l'UTT(Y) ou hors de l'UTT(N)</br>
-Profil: Y (oui) ou N (non), ce qui permet de savoir si l’élément de formation appartient au profil</br>
-de la branche ou de la filière. Cet attribut permet de valider (ou pas) les crédits du TCBR et de
-la filière.</br>
-Credit: nombre de crédits obtenus</br>
-Résultat:A, ...F, ABS, RES, ADM, ...</br>
-          </p>
-      </div>
-      <p class="lead">
-      <div >
-          <form class="form-signin" action="addStudentAction.php">
-
-              <table class="table table-striped">
-                  <thead>
-                      <tr>
-                          <th>Num</th>
-                          <th>Numéro Semestre</th>
-                          <th>Label Semestre</th>
-                          <th>Sigle</th>
-                          <th>Catégorie</th>
-                          <th>Affectation</th>
-                          <th>Utt?</th>
-                          <th>Profil</th>
-                          <th>Credit</th>
-                          <th>Résultat</th>
-
-                      </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>45862</td>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>TC</td>
-                            <td>MPL</td>
-                        </tr>
-                        <tr>
-                            <td>45993</td>
-                            <td>Jacob</td>
-                            <td>Thornton</td>
-                            <td>BR</td>
-                            <td>?</td>
-                        </tr>
-                        <tr>
-                            <td>86521</td>
-                            <td>Larry</td>
-                            <td>the Bird</td>
-                            <td>BR</td>
-                            <td>?</td>
-                        </tr>
-
-
-
-
-                    </tbody>
-
-                </table>
-
-
-
-
-
-
-                <div class="forceSize">
-                    <button class="btn btn-lg btn-primary btn-block" type="submit">Envoyer</button>
-                </div>
-            </form>
-
-
-
-
-            </p>
-        </div>
+    <div class="mainText">
+        <h1>Votre parcours</h1><br>
     </div>
+
+
+    <form class="form-signin" action="addStudentAction.php">
+
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Num</th>
+                    <th>Num Semestre</th>
+                    <th>Label Semestre</th>
+                    <th>Sigle</th>
+                    <th>Catégorie</th>
+                    <th>Affectation</th>
+                    <th>Utt?</th>
+                    <th>Profil</th>
+                    <th>Credit</th>
+                    <th>Résultat</th>
+
+                </tr>
+            </thead>
+
+            <tbody>
+
+                <tr>
+
+                    <td>1</td>
+                    <td><input type="text" class="form-control input-sm" style="width: 40%;" name="numsem1"></td>
+                    <td><input type="text" class="form-control input-sm" style="width: 80%;" name="label1"></td>
+                    <td><input type="text" class="form-control input-sm" style="width: 40%;" name="sigle1"></td>
+                    <td><input type="text" class="form-control input-sm" style="width: 40%;" name="categorie1"></td>
+                    <td><input type="text" class="form-control input-sm" style="width: 40%;" name="affectation1"></td>
+                    <td><select class="form-control" name="utt1">
+                            <option>Oui</option>
+                            <option>Non</option>
+                        </select></td>
+                    <td><select class="form-control" name="profil1">
+                            <option>Oui</option>
+                            <option>Non</option>
+                        </select></td>
+                    <td><input type="text" class="form-control input-sm" style="width: 40%;" name="credit1"></td>
+                    <td><input type="text" class="form-control input-sm" style="width: 40%;" name="resultat1"></td>
+
+                </tr>
+
+
+            </tbody>
+            <tbody id="container"/>
+
+        </table>
+        <button id="trigger" onclick="addFields()" class="btn btn-info" type="button">Ajouter ligne</button>
+
+
+
+
+
+
+        <div class="forceSize">
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Envoyer</button>
+        </div>
+    </form>
+
+
+
+
+</p>
+</div>
+
+<div class="well" style="text-align: left">
+    <p><ul class="list-group">
+        <li class="list-group-item"><b>Num Semestre :</b> numéro de votre semestre à l'UTT</li>
+        <li class="list-group-item"><b>Label Semestre :</b> sigle d'une branche(TC,ISI,SRT,...) et numéro du semestre dans cette branche</li>
+        <li class="list-group-item"><b>Sigle : </b>sigle d’une UE, label d’un stage, ...</li>
+        <li class="list-group-item"> <b>Catégorie :</b> CS, TM, EC,HT, ME, ST, SE, HP, NPML,...</li>
+        <li class="list-group-item">  <b>Affectation :</b> à quelle partie du cursus doivent être affectés les crédits associés à l’élément de formation.</li>
+        <li class="list-group-item">  <b>Utt? : </b>fait à l'UTT(Y) ou hors de l'UTT(N).</li>
+        <li class="list-group-item">  <b>Profil :</b> Y (oui) ou N (non), ce qui permet de savoir si l’élément de formation appartient au profil.
+            de la branche ou de la filière. Cet attribut permet de valider (ou pas) les crédits du TCBR et de
+            la filière.</li>
+        <li class="list-group-item">    <b>Credit :</b> nombre de crédits obtenus.</li>
+        <li class="list-group-item">     <b>Résultat :</b> A, ...F, ABS, RES, ADM, ...</li>
+        </p></ul>
+</div>
 </div>
 
 </body>
