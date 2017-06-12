71edb660bf3988f8c1c38e6167d6f4441d94c63a
Malathrom <adrian.genin@live.fr>
2017-06-12 19:15:14 +0200

    ble

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php include "header.php" ?>
+<form action="test2.php">
+    <label class="btn btn-default btn-file">
+        Browse <input name="csv" type="file" hidden>
+    </label>
+    <input type="submit" value="Envoyer" />
+</form>
